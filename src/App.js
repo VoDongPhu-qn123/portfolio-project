5542e8ceb0569fa94bda27a178906424521ec9e4
VoDongPhu-qn123 <phuuit169@gmail.com>
2024-08-31 00:14:53 +0700

    NavBar UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <div style={{ height: "1500px" }}></div>
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,18 +1,32 @@
 import React from "react";
+import { Link } from "react-scroll";
 import "./NavBar.css";
 import logo from "../../assets/logo.png";
 import contact from "../../assets/contact.png";
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav className="navBar">
       <a href="/">
         <img src={logo} alt="logo" className="logo" />
       </a>
-      <div className="deskTopMenu"></div>
+      <div className="deskTopMenuList">
+        <Link to="" smooth={true} duration={500} className="deskTopMenuItem">
+          Home
+        </Link>
+        <Link to="" smooth={true} duration={500} className="deskTopMenuItem">
+          About
+        </Link>
+        <Link to="" smooth={true} duration={500} className="deskTopMenuItem">
+          Portfolio
+        </Link>
+        <Link to="" smooth={true} duration={500} className="deskTopMenuItem">
+          Clients
+        </Link>
+      </div>
       <button className="deskTopMenuBtn">
-        <img src={contact} alt="contact" />
-        Contact me!
+        <img src={contact} alt="contact" className="deskTopMenuImg" />
+        Contact me
       </button>
     </nav>
   );
